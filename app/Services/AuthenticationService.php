3767e41acf6215cd8cca4bdc0f46d8e65ef921d2
Alan <tan_alan1020@hotmail.com>
2019-12-29 01:53:37 +0800

    add authentication service

---
+++ app/Services/AuthenticationService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Services;
+
+use Mail;
+use App\Mail\Auth\AuthEmail;
+
+class AuthenticationService
+{
+    public function sendEmail($title, $message, $email_address)
+    {
+        $title = "Title test";
+        $message = "This is a test!";
+        $email_address = "tan_alan1020@hotmail.com";
+
+        $data = [
+            'title' => $title,
+            'message' => $message
+        ];
+
+        Mail::to($email_address)->send(new AuthEmail($data));
+        return true;
+    }
+}
